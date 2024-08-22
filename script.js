5a695a462d0454f78dc040058b2380a770999545
Harinii24 <138215679+HARINII2415@users.noreply.github.com>
2024-08-22 15:30:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
+var typed = new Typed(".text", {
+    strings: ["Programming" , "Cybersecurity" , "Web Development"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+});
 
+
+const toTop = document.querySelector(".top");
+window.addEventListener("scroll",() =>{
+    if (window.pageYOffset > 100){
+        toTop.classList.add("active");
+    }
+    else{
+        toTop.classList.remove("active");
+    }
+})
 /*=========Navbar Scroll===========*/
 document.addEventListener("DOMContentLoaded", function() {
     const navbar = document.getElementById("header");
